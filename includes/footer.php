8c8cdc63dcfd82753a246f121306b57f653485ee
SEEMULLERJ_INFO <SEEMULLERJ_INFO@10.134.98.73>
2014-11-25 13:26:34 +0100

    Ajout de contenu dans les fichiers includes suivants :
    	+ header_logged_in.php
    	+ header_not_logged_in.php
    	+ header_logged_in_admin.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,8 +1,5 @@
-<?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+<footer class="footer">
+    <div class="container">
+        <p class="text-muted">INSERER DU CONTENU ICI</p>
+    </div>
+</footer>
--- includes/header_logged_in.php
+++ includes/header_logged_in.php
@@ -1,8 +1,30 @@
-<?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+<!-- NAVBAR & LOGGED IN USER-->
+<nav class="navbar navbar-default navbar-static-top" role="navigation">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-controls="navbar">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="maquette.html">Ani'Sound</a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse" aria-expanded="false" style="height: 1px;">
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="maquette.html">Acceuil</a></li>
+                <li><a href="#game">Mini-jeux</a></li>
+            </ul>
+            <form class="navbar-form navbar-right" role="form">
+                <ul class="nav nav-pills" role="tablist">
+                    <li role="presentation" class="active">
+                        <a href="#"> <!-- INSERT USERPAGE IN HREF -->
+                            <i>USERNAME HERE</i>
+                            <span class="badge"><!--INSERT NUMBER HERE FOR REQUESTS ( LEAVE EMPTY FOR USERS ) --></span>
+                        </a>
+                    </li>
+                </ul>
+            </form>
+        </div>
+    </div>
+</nav>
