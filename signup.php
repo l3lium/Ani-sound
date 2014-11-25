166d5c18e94676a147c0d88e7d605628d2bd81f8
SEEMULLERJ_INFO <SEEMULLERJ_INFO@10.134.98.73>
2014-11-25 16:06:36 +0100

    Modifications signup

--- signup.php
+++ signup.php
@@ -1,3 +1,11 @@
+<?php
+require_once 'includes/specific_bdd.php';
+require_once 'includes/struct.php';
+
+if (isConnected()) {
+    header('Location: index.php');
+}
+?>
 <!DOCTYPE html>
 <html lang="en"><head>
         <meta charset="utf-8">
@@ -13,35 +21,9 @@
 
     <body>
         <!-- NAVBAR -->
-        <nav class="navbar navbar-default navbar-static-top" role="navigation">
-            <div class="container">
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-controls="navbar">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                    <a class="navbar-brand" href="maquette.html">Ani'Sound</a>
-                </div>
-                <div id="navbar" class="navbar-collapse collapse" aria-expanded="false" style="height: 1px;">
-                    <ul class="nav navbar-nav">
-                        <li class="active"><a href="maquette.html">Acceuil</a></li>
-                        <li><a href="#game">Mini-jeux</a></li>
-                    </ul>
-                    <form class="navbar-form navbar-right" role="form">
-                        <div class="form-group">
-                            <input type="text" placeholder="Username" class="form-control">
-                        </div>
-                        <div class="form-group">
-                            <input type="password" placeholder="Mot-de-passe" class="form-control">
-                        </div>
-                        <button name="login" type="submit" class="btn btn-success">Se connecter  <span class="glyphicon glyphicon-lock"></span></button>
-                        <a class="btn btn-primary" href="signup.html">S'enregistrer <span class="glyphicon glyphicon-user"></span></a>
-                    </form>
-                </div>
-            </div>
-        </nav>
+        <?php
+        getHeader();
+        ?>
         <!-- CONTAINER -->
         <div class="container">
             <div class="center-block">
@@ -62,4 +44,8 @@
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
         <script src="./js/bootstrap.min.js"></script>
         <script src="./js/ie10-viewport-bug-workaround.js"></script>
-    </body></html>
+    </body>
+    <?php
+    getFooter();
+    ?>
+</html>
