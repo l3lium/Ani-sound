0c00b8c82d019e99419474ec10de52f7c4e504ab
SEEMULLERJ_INFO <SEEMULLERJ_INFO@10.134.98.73>
2014-11-25 15:09:04 +0100

    Ajout des fichiers et dossiers  suivants : 	+ [FICHIER] footer_logged_in.php 	+ [FICHIER] footer_logged_in_admin.php 	+ [FICHIER] footer_not_logged_in.php 	+ [DOSSIER] maquette
    Suppression des fichiers suivants:
    	- footer.php
    
    Modification des fichiers suivants (Implémentation header & footer automatiques)
    	+ index.php

--- includes/struct.php
+++ includes/struct.php
@@ -1,5 +1,41 @@
 <?php
 
-function getHeader(){
-    include '../includes/header_logged_in.php';
+/*
+  ======Specific Bdd======
+  Auteur: 	Oliveira Stéphane & Seemuller Julien
+  Classe: 	I.IN-P4B
+  Date:		25/11/2014
+  Version:	0.1
+  Description:    Script regroupant les fonctions spécifiques au site web et la base de donnée
+ */
+require_once 'specific_bdd.php';
+
+function getHeader() {
+    //On affiche différents headers en fonction de la personne qui est connectée
+    //Si personne n'est connecté, on affiche un header donnant la possibilité de s'inscrire et de se logger.
+    if (isConnected()) {
+        if (isAdmin()) {
+            include 'structure/header_logged_in_admin';
+        } else {
+            include 'structure/header_logged_in';
+        }
+    } 
+    else {
+        include 'structure/header_not_logged_in.php';
+    }
+}
+
+function getFooter() {
+    //On affiche différents footers en fonction de la personne qui est connectée
+    //Si personne n'est connecté, on affiche un footer par défaut
+    if (isConnected()) {
+        if (isAdmin()) {
+            include 'structure/footer_logged_in_admin';
+        } else {
+            include 'structure/footer_logged_in';
+        }
+    } 
+    else {
+        include 'structure/footer_not_logged_in.php';
+    }
 }
---
+++ includes/structure/footer_logged_in_admin.php
@@ -0,0 +1,5 @@
+<footer class="footer">
+    <div class="container">
+        <p class="text-muted">INSERER DU CONTENU ICI</p>
+    </div>
+</footer>
