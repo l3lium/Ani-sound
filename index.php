81fce4f7134a2e783d561251df2eb2da2b0d46c4
SEEMULLERJ_INFO <SEEMULLERJ_INFO@10.134.98.73>
2014-11-25 14:13:32 +0100

    Ajout des fichiers  suivants : 	+ index.php 	+ signup.php

---
+++ index.php
@@ -0,0 +1,181 @@
+<!DOCTYPE html>
+<html lang="en"><head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <title>Ani'Sound</title>
+        <link href="./css/bootstrap.min.css" rel="stylesheet">
+        <link href="./css/style.css" rel="stylesheet">
+        <script src="./js/ie-emulation-modes-warning.js"></script>
+    </head>
+
+    <body>
+        <!-- NAVBAR -->
+        <nav class="navbar navbar-default navbar-static-top" role="navigation">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-controls="navbar">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="maquette.html">Ani'Sound</a>
+                </div>
+                <div id="navbar" class="navbar-collapse collapse" aria-expanded="false" style="height: 1px;">
+                    <ul class="nav navbar-nav">
+                        <li class="active"><a href="maquette.html">Acceuil</a></li>
+                        <li><a href="#game">Mini-jeux</a></li>
+                    </ul>
+                    <form class="navbar-form navbar-right" role="form">
+                        <div class="form-group">
+                            <input type="text" placeholder="Username" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <input type="password" placeholder="Mot-de-passe" class="form-control">
+                        </div>
+                        <button name="login" type="submit" class="btn btn-success">Se connecter  <span class="glyphicon glyphicon-lock"></span></button>
+                        <a class="btn btn-primary" href="signup.html">S'enregistrer <span class="glyphicon glyphicon-user"></span></a>
+                    </form>
+                </div>
+            </div>
+        </nav>
+        <!-- CONTAINER -->
+        <div class="container">
+            <div class="center-block">
+                <!-- CONTAINER PANELS ANIMAUX -->
+                <div class="row">
+                    <div class="col-sm-3">
+                        <div class="panel panel-success">
+                            <div class="panel-body">
+                                <img class="img-thumbnail center-block" alt="thumbnail" src="./img/placeholder.png" data-holder-rendered="true" style="width: 200px; height: 200px;">
+                            </div>
+                            <div class="panel-heading">
+                                <h3 class="panel-title">Nom de l'animal</h3>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-3">
+                        <div class="panel panel-success">
+                            <div class="panel-body">
+                                <img class="img-thumbnail center-block" alt="thumbnail" src="./img/placeholder.png" data-holder-rendered="true" style="width: 200px; height: 200px;">
+                            </div>
+                            <div class="panel-heading">
+                                <h3 class="panel-title">Nom de l'animal</h3>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-3">
+                        <div class="panel panel-success">
+                            <div class="panel-body">
+                                <img class="img-thumbnail center-block" alt="thumbnail" src="./img/placeholder.png" data-holder-rendered="true" style="width: 200px; height: 200px;">
+                            </div>
+                            <div class="panel-heading">
+                                <h3 class="panel-title">Nom de l'animal</h3>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-3">
+                        <div class="panel panel-success">
+                            <div class="panel-body">
+                                <img class="img-thumbnail center-block" alt="thumbnail" src="./img/placeholder.png" data-holder-rendered="true" style="width: 200px; height: 200px;">
+                            </div>
+                            <div class="panel-heading">
+                                <h3 class="panel-title">Nom de l'animal</h3>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-3">
+                        <div class="panel panel-success">
+                            <div class="panel-body">
+                                <img class="img-thumbnail center-block" alt="thumbnail" src="./img/placeholder.png" data-holder-rendered="true" style="width: 200px; height: 200px;">
+                            </div>
+                            <div class="panel-heading">
+                                <h3 class="panel-title">Nom de l'animal</h3>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-3">
+                        <div class="panel panel-success">
+                            <div class="panel-body">
+                                <img class="img-thumbnail center-block" alt="thumbnail" src="./img/placeholder.png" data-holder-rendered="true" style="width: 200px; height: 200px;">
+                            </div>
+                            <div class="panel-heading">
+                                <h3 class="panel-title">Nom de l'animal</h3>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-3">
+                        <div class="panel panel-success">
+                            <div class="panel-body">
+                                <img class="img-thumbnail center-block" alt="thumbnail" src="./img/placeholder.png" data-holder-rendered="true" style="width: 200px; height: 200px;">
+                            </div>
+                            <div class="panel-heading">
+                                <h3 class="panel-title">Nom de l'animal</h3>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-3">
+                        <div class="panel panel-success">
+                            <div class="panel-body">
+                                <img class="img-thumbnail center-block" alt="thumbnail" src="./img/placeholder.png" data-holder-rendered="true" style="width: 200px; height: 200px;">
+                            </div>
+                            <div class="panel-heading">
+                                <h3 class="panel-title">Nom de l'animal</h3>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-3">
+                        <div class="panel panel-success">
+                            <div class="panel-body">
+                                <img class="img-thumbnail center-block" alt="thumbnail" src="./img/placeholder.png" data-holder-rendered="true" style="width: 200px; height: 200px;">
+                            </div>
+                            <div class="panel-heading">
+                                <h3 class="panel-title">Nom de l'animal</h3>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-3">
+                        <div class="panel panel-success">
+                            <div class="panel-body">
+                                <img class="img-thumbnail center-block" alt="thumbnail" src="./img/placeholder.png" data-holder-rendered="true" style="width: 200px; height: 200px;">
+                            </div>
+                            <div class="panel-heading">
+                                <h3 class="panel-title">Nom de l'animal</h3>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-3">
+                        <div class="panel panel-success">
+                            <div class="panel-body">
+                                <img class="img-thumbnail center-block" alt="thumbnail" src="./img/placeholder.png" data-holder-rendered="true" style="width: 200px; height: 200px;">
+                            </div>
+                            <div class="panel-heading">
+                                <h3 class="panel-title">Nom de l'animal</h3>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-3">
+                        <div class="panel panel-success">
+                            <div class="panel-body">
+                                <img class="img-thumbnail center-block" alt="thumbnail" src="./img/placeholder.png" data-holder-rendered="true" style="width: 200px; height: 200px;">
+                            </div>
+                            <div class="panel-heading">
+                                <h3 class="panel-title">Nom de l'animal</h3>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <button type="button" class="btn btn-lg btn-info center-block">Afficher Plus<span class="glyphicon glyphicon-chevron-down"></span></button>
+            </div>
+        </div>
+
+        <!-- Bootstrap core JavaScript
+        ================================================== -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+        <script src="./js/bootstrap.min.js"></script>
+        <script src="./js/ie10-viewport-bug-workaround.js"></script>
+    </body></html>
---
+++ signup.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en"><head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <title>Ani'Sound</title>
+        <link href="./css/bootstrap.min.css" rel="stylesheet">
+        <link href="./css/style.css" rel="stylesheet">
+        <script src="./js/ie-emulation-modes-warning.js"></script>
+    </head>
+
+    <body>
+        <!-- NAVBAR -->
+        <nav class="navbar navbar-default navbar-static-top" role="navigation">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-controls="navbar">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="maquette.html">Ani'Sound</a>
+                </div>
+                <div id="navbar" class="navbar-collapse collapse" aria-expanded="false" style="height: 1px;">
+                    <ul class="nav navbar-nav">
+                        <li class="active"><a href="maquette.html">Acceuil</a></li>
+                        <li><a href="#game">Mini-jeux</a></li>
+                    </ul>
+                    <form class="navbar-form navbar-right" role="form">
+                        <div class="form-group">
+                            <input type="text" placeholder="Username" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <input type="password" placeholder="Mot-de-passe" class="form-control">
+                        </div>
+                        <button name="login" type="submit" class="btn btn-success">Se connecter  <span class="glyphicon glyphicon-lock"></span></button>
+                        <a class="btn btn-primary" href="signup.html">S'enregistrer <span class="glyphicon glyphicon-user"></span></a>
+                    </form>
+                </div>
+            </div>
+        </nav>
+        <!-- CONTAINER -->
+        <div class="container">
+            <div class="center-block">
+                <!-- CONTAINER LOGIN -->
+                <form class="form-signin" method="post" action="signup_test.php">
+                    <h3>Remplir les champs suivants</h3><hr/>
+                    <label class="">Pseudo:</label><input class="form-control" name="username" type="text" placeholder="ex : ''Johndoe''"/><br/>
+                    <label>Email:</label><input class="form-control" name="email" type="text" placeholder="ex : ''john.doe@placeholder.com''"/><br/> 
+                    <label>Password :</label><input class="form-control" name="password" type="password"/><br/>
+                    <label>Password confirm :</label><input class="form-control" name="passwordConfirm" type="password"/><br/>
+                    <button name="signup" class="btn btn-lg btn-primary btn-block" type="submit">S'inscrire</button>
+                </form>
+            </div>
+        </div>
+
+        <!-- Bootstrap core JavaScript
+        ================================================== -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+        <script src="./js/bootstrap.min.js"></script>
+        <script src="./js/ie10-viewport-bug-workaround.js"></script>
+    </body></html>
