47a4e870de765a73e15cb5b178efe8d6bf0b0479
Julien Seemuller <Julien Seemuller@WafflePC>
2014-12-16 01:11:26 +0100

    Mise a jour des données des animaux sur base de données et ajout de photos dans le dossier up-content

--- bdd/animaltest_data.sql
+++ bdd/animaltest_data.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  127.0.0.1
--- Généré le :  Jeu 27 Novembre 2014 à 13:20
+-- Généré le :  Mar 16 Décembre 2014 à 01:09
 -- Version du serveur :  5.6.15-log
 -- Version de PHP :  5.4.24
 
@@ -21,25 +21,27 @@ SET time_zone = "+00:00";
 --
 
 --
--- Vider la table avant d'insérer `animal`
---
-
-TRUNCATE TABLE `animal`;
---
 -- Contenu de la table `animal`
 --
 
 INSERT INTO `animal` (`id`, `name`, `img`, `sound`, `date`, `pending`, `idUser`) VALUES
-(1, 'sasa', 'fdsafdsaf', 'dsafdsafdsafd', '2014-11-25 14:56:51', 1, 3),
-(2, 'sasafgdfd', 'fdsafdsaf', 'dsafdsafdsafd', '2014-11-25 14:56:51', 1, 2),
-(3, 'safdsfdsadsa', 'sfdsafdsafdsafdsafsda', 'fdsafdsafdsafdsafsa', '2014-11-25 14:56:51', 0, 3),
-(4, 'gfdgdfgdf', 'gfdsgfdsg', 'fdsgfdsgfds', '2014-11-25 14:56:51', 1, 5),
-(5, 'fdsfsdaf4', '4ffa4f4a', 'f4afawf4waf', '2014-11-25 14:56:51', 1, 4),
-(6, 'gsg5rg4tgrg', 'e54g54tg5fv5gf', 'g54g54zt5wg5g5t', '2014-11-25 14:56:51', 1, 4),
-(7, '43wqfgrstg', '45tg54g5tg', '54g54g54', '2014-11-25 14:56:51', 1, 3),
-(8, 'greatgr4ge54g5e4', 'gw43gfe5g5eg5eg5e', 'g5wg5ew4tg54g54e', '2014-11-25 14:56:51', 1, 2),
-(9, 'grsetgrsegdrghdr', 'grfdtgsertgrdtg', 'erstfrestg4r', '2014-11-25 14:56:51', 1, 3),
-(11, 'test', 'img', 'sound', '2014-11-25 15:59:36', 1, 2);
+(1, 'Cheval', 'up-content/img/animal/cheval.jpg', 'up-content/sound/animal/cheval.mp3', '2014-11-25 14:56:51', 0, 3),
+(2, 'Ours', 'up-content/img/animal/ours.jpg', 'up-content/sound/animal/cheval.mp3', '2014-11-25 14:56:51', 0, 2),
+(3, 'Koala', 'up-content/img/animal/koala.jpg', 'up-content/sound/animal/cheval.mp3', '2014-11-25 14:56:51', 0, 3),
+(4, 'Panda', 'up-content/img/animal/panda.jpg', 'up-content/sound/animal/cheval.mp3', '2014-11-25 14:56:51', 0, 5),
+(5, 'Lion', 'up-content/img/animal/lion.jpg', 'up-content/sound/animal/cheval.mp3', '2014-11-25 14:56:51', 1, 4),
+(6, 'Lapin', 'up-content/img/animal/lapin.jpg', 'up-content/sound/animal/cheval.mp3', '2014-11-25 14:56:51', 0, 4),
+(7, 'Chat', 'up-content/img/animal/chat.jpg', 'up-content/sound/animal/cheval.mp3', '2014-11-25 14:56:51', 0, 3),
+(8, 'Chien', 'up-content/img/animal/chien.jpg', 'up-content/sound/animal/cheval.mp3', '2014-11-25 14:56:51', 0, 2),
+(9, 'Aigle', 'up-content/img/animal/aigle.jpg', 'up-content/sound/animal/cheval.mp3', '2014-11-25 14:56:51', 1, 3),
+(13, 'Vache', 'up-content/img/animal/vache.jpg', 'up-content/sound/animal/cheval.mp3', '2014-12-15 23:47:10', 1, 0),
+(12, 'Dauphin', 'up-content/img/animal/dauphin.jpg', 'up-content/sound/animal/cheval.mp3', '2014-12-15 23:47:10', 1, 0),
+(14, 'Hippopotame', 'up-content/img/animal/hippopotame.jpg', 'up-content/sound/animal/cheval.mp3', '2014-12-15 23:48:05', 1, 0),
+(15, 'Perroquet', 'up-content/img/animal/perroquet.jpg', 'up-content/sound/animal/cheval.mp3', '2014-12-15 23:48:05', 1, 0),
+(16, 'Perruche', 'up-content/img/animal/perruche.jpg', 'up-content/sound/animal/cheval.mp3', '2014-12-15 23:51:50', 1, 0),
+(17, 'Hibou', 'up-content/img/animal/hibou.jpg', 'up-content/sound/animal/cheval.mp3', '2014-12-15 23:51:50', 1, 0),
+(18, 'Phoque', 'up-content/img/animal/seal.jpg', 'up-content/sound/animal/cheval.mp3', '2014-12-15 23:54:44', 0, 0),
+(19, 'Loutre', 'up-content/img/animal/loutre.jpg', 'up-content/sound/animal/cheval.mp3', '2014-12-15 23:54:44', 0, 0);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
