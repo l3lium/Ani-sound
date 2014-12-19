8384cb946fb45aaab744df63e68936297d075523
Stéphane <Stphane@L3lith-PC>
2014-12-19 01:14:54 +0100

    Finalisation MiniJeu 	Ajout de nouveau styles dans le fichier css [style.css] 	Finalisation du jeu et corrections de bug [minigame.js]

--- js/minigame.js
+++ js/minigame.js
@@ -1,41 +1,172 @@
-var animals;
+/*
+======Minigame=======
+Auteur: 	Oliveira Stéphane & Seemuller Julien
+Classe: 	I.IN-P4B
+Date:		16/12/2014
+Version:	0.1
+*/
 
-//animation de suppression
-function slideLeftDelete(name) {
-    $('#' + name).animate({
-        opacity: 0,
-        marginLeft: "120"
-    }, 'slow').delay(800).remove;
-}
+var animalPanel = "<div class=\"col-sm-4 animal\">" +
+        "<div class=\"panel panel-success\">" +
+        "<div class=\"panel-body\">" +
+        "<img id=\"animal-img1\" class=\"img-thumbnail center-block img-animal\" alt=\"thumbnail\" src=\"\" data-holder-rendered=\"true\" style=\"width: 200px; height: 200px;\">  " +
+        "</div>" +
+        "<div class=\"panel-heading\">" +
+        "<h3 class=\"panel-title\"></h3>" +
+        "</div>" +
+        "</div>" +
+        "</div>";
 
-function drawAnimal(arrayAnimal) {
-    arrayOutput = new Array;
-
-    for (i = 0; i < 3; i++) {
-        arrayOutput[i] = arrayAnimal[Math.round(Math.random(arrayAnimal.length))];
-    }
-    return arrayOutput;
-}
+var game = new Game();
 
 $(document).ready(function() {
     $("#startGame").click(function() {
-
-        var xhr = new XMLHttpRequest();
-        var url = $(this).attr('data-url');
-        var params = "next=";
-        alert(url);
-        xhr.open("POST", url, true);
-        xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-        //xhr.setRequestHeader("Content-length", params.length);
-        //xhr.setRequestHeader("Connection", "close");
-        xhr.send(params);
-        xhr.onreadystatechange = function(event) {
-            if (xhr.readyState === 4) {
-                alert("done")
-                animals = JSON.parse(event.target.responseText);
-                console.log(animals);
-            }
-        };
+        game.start();
     });
 }
 );
+function Game() {
+    var localthis = this;
+    this.allAnimals = new Array();
+    this.animals = new Array();
+    this.randAnimal = new Array();
+    this.gameDom;
+    
+    /**Game.start
+     * Initialise l'objet Game
+     */
+    this.start = function() {
+        this.loadAnimals();
+    };
+    
+    /**Game.replay
+     * Initialise une nouvelle partie
+     */
+    this.replay = function() {
+        this.animals = new Array();
+        for (i = 0; i < 3; i++) {
+            //Tirage aléatoire d'un animal dans le tableau allAnimals executé 3 fois
+            var id = Math.round(Math.random() * (this.allAnimals.length - 1));
+            this.animals.push(this.allAnimals[id]);
+        }
+        //Tirage aléatoire d'un animal parmis les trois
+        id = Math.round(Math.random() * (this.animals.length - 1));
+        this.randAnimal = this.animals[id];
+
+        //Changement d'écran de jeu
+        $('#instructions').hide();
+        $('#game').fadeIn();
+
+        //Chargement et initialisation des elements
+        this.changeAnimal();
+        this.setEvent();
+        
+        //Chargement du son
+        this.loadSound();
+    };
+    
+    /**Game.setEvent
+     * Ajoute tout les evenements sur les différents elements de la page
+     */
+    this.setEvent = function() {
+        $("#dropzone").on("drop", function(event) {
+            event.preventDefault();
+            var data = '#' + event.originalEvent.dataTransfer.getData("text");
+            event.target.appendChild($(data).get(0));//Récupération du node avec get(0)
+            localthis.eventOff();
+            localthis.checkAnimal($(data).data("id"));
+        }).on("dragover", function(event) {
+            event.preventDefault();
+        }).children().on("dragover", function(event) {//Evite 
+            event.stopPropagation();
+        });
+
+        $('#replaySound').click(function() {
+            localthis.playSound();
+        });
+
+        $('.replay').click(function() {
+            localthis.replay();
+            //$(".animal").remove();
+            $("#dropzone").children().remove();
+
+            $("#victory, #defeat").hide();
+        });
+    };
+    
+    /**Game.setEvent
+     * Retire tout les evenements des différents elements de la page
+     */
+    this.eventOff = function() {
+        $("#dropzone").off();
+        $(".animal").off();
+    }
+    
+    /**Game.loadSound
+     * Change le fichier source de la balise audio
+     */
+    this.loadSound = function() {
+        var audio = $("#game > audio");
+        $("#game > audio").attr("src", this.randAnimal.sound);
+        this.playSound();
+    };
+    
+    /**Game.playSound
+     * Joue le son de l'animal
+     */
+    this.playSound = function() {
+        var audio = $("#game > audio");
+        audio.trigger("play");
+    }
+    
+    /**Game.checkAnimal
+     * Vérifie la réponse de l'utilisateur
+     */
+    this.checkAnimal = function(id) {
+        //Affiche le message de victoire ou de defaite
+        if (id == this.randAnimal.id) {
+            $("#victory").show();
+        } else {
+            $("#defeat").show();
+        }
+    };
+    
+    /**Game.changeAnimal
+     * Met à jour les nouveaux animaux de la page
+     */
+    this.changeAnimal = function() {
+        $(".animal").remove();//Suppresion des anciens panel d'animaux
+        for (i = 0; i < 3; i++) {
+            //Création des nouveaux panel d'animaux
+            var element = $(animalPanel).clone();
+            $(element).attr("id", "animal" + i).appendTo("#animals");
+            //Definission 
+            element.find(".panel-body > img").attr("src", this.animals[i].img);
+            element.find(".panel-body > img").data("id", this.animals[i].id);
+            element.find(".panel-heading > h3").text(this.animals[i].name);
+
+            //Ajout d'evenement au commencement du deplacement de l'element
+            element.find("img").bind("dragstart", function(event) {
+                event.originalEvent.dataTransfer.setData("text", event.target.id);
+            });
+        }
+    };
+
+    /**Game.loadAnimals
+     * Recupere la liste des animaux dans la bdd
+     */
+    this.loadAnimals = function() {
+        var xhr = new XMLHttpRequest();
+        var url = $("#startGame").attr('data-url');//Récupération de l'url
+        xhr.open("POST", url, true);
+        xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        xhr.send();
+        xhr.onreadystatechange = function(event) {
+            if (xhr.readyState === 4) {
+                localthis.ready = true;
+                localthis.allAnimals = JSON.parse(event.target.responseText);
+                localthis.replay();
+            }
+        };
+    };
+}
--- miniJeu.php
+++ miniJeu.php
@@ -1,6 +1,14 @@
 <?php
 require_once 'includes/specific_funtions.php';
 require_once 'includes/struct.php';
+
+//Connexion utilisateur
+if (filter_input(INPUT_POST, 'login')) {
+    $pseudo = filter_input(INPUT_POST, 'username');
+    $pass = filter_input(INPUT_POST, 'password');
+
+    userConnect($pseudo, $pass);
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -36,20 +44,44 @@ require_once 'includes/struct.php';
                 <h1>Trouvez l'animal</h1><hr/>
                 <article class="center-block">
                     <div class="jumbotron">
-                        <h2>Instructions</h2><hr/>
-                        <p>
-                            Pour jouer au jeu, appuyez sur le bouton rouge. Soyez sur d'avoir une source audio a disposition.
-                            Puis, écoutez le cri de l'animal, cliquez ensuite sur l'animal auquel le cri appartient.
-                            Si le cri correspond bien à l'animal choisi, le jeu vous affichera une confirmation, sinon un
-                            écran "Game Over" vous donnera la possibilité de rejouer
-                        </p><br>
-                        <button id="startGame" type="button" data-url="includes/structure/allAnimals.php" class="btn btn-lg btn-danger center-block">Démarrer le jeu</button>
+                        <div id="instructions">
+                            <h2>Instructions</h2><hr/>
+                            <p>
+                                Pour jouer au jeu, appuyez sur le bouton rouge. Soyez sur d'avoir une source audio a disposition.
+                                Puis, écoutez le cri de l'animal, cliquez ensuite sur l'animal auquel le cri appartient.
+                                Si le cri correspond bien à l'animal choisi, le jeu vous affichera une confirmation, sinon un
+                                écran "Game Over" vous donnera la possibilité de rejouer
+                            </p><br>
+                            <button id="startGame" type="button" data-url="includes/structure/allAnimals.php" class="btn btn-lg btn-danger center-block">Démarrer le jeu</button>
+                        </div>
+                        <div id="game">
+                            <audio>
+
+                            </audio>
+                            <div id="victory" class="alert-success">
+                                <h1>Bravo les copains !</h1>
+                                <button type="button" data-url="includes/structure/content_animals.php" class="btn btn-lg btn-warning center-block replay">Rejouer  <span class="glyphicon glyphicon-refresh"></span></button>
+                            </div>
+                            <div id="defeat" class="alert-danger">
+                                <h1>Perdu les amis !</h1>
+                                <button type="button" data-url="includes/structure/content_animals.php" class="btn btn-lg btn-warning center-block replay">Rejouer  <span class="glyphicon glyphicon-refresh"></span></button>
+                            </div>
+                            <h2>Déposez un animal dans le cadre</h2>
+                            <div class="panel-body" id="dropzone" class="row">
+
+                            </div>
+                            <button id="replaySound" type="button" data-url="includes/structure/content_animals.php" class="btn btn-lg btn-success center-block">Rejouer le son  <span class="glyphicon glyphicon-repeat"></span></button>
+                            <div id="animals" class="row">
+                            </div>
+                        </div>
                     </div>
-                </article>
             </div>
-        </div>
-        <?php
-        getFooter();
-        ?>
-    </body>
+        </article>
+    </div>
+    <?php
+    getFooter();
+    ?>
+</div>
+
+</body>
 </html>
